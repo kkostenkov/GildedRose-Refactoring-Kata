ec3a3d318e0ef5bb0611a01c8f7da4bb552abb98
Vladimir Dukhovoi <vdukhovoi@gmail.com>
2023-09-27 21:06:52 +0300

    Add some tests

--- GildedRose/Tests/GildedRoseTest.cs
+++ GildedRose/Tests/GildedRoseTest.cs
@@ -12,7 +12,87 @@ namespace csharp
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void Should_DecreaseQuality_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].Quality);
+        }
+        
+        [Test]
+        public void Should_DecreaseSellIn_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].SellIn);
+        }
+        
+        [Test]
+        public void Should_DecreaseQualityTwiceAsFast_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+        }
+        
+        [Test]
+        public void Should_QualityNoneNegative_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+        
+        [Test]
+        public void Should_IncreaseQualityBrie_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(11, Items[0].Quality);
+        }
+        
+        [Test]
+        public void Should_KeepQualityNoHigher50_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+        
+        [Test]
+        public void Should_KeepSulfurasValues_When_Updated()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(80, Items[0].Quality);
+            Assert.AreEqual(10, Items[0].SellIn);
+        }
+        
+        [TestCase(12, 3, ExpectedResult = 4)]
+        [TestCase(11, 3, ExpectedResult = 4)]
+        [TestCase(10, 3, ExpectedResult = 5)]
+        [TestCase(9, 3, ExpectedResult = 5)]
+        [TestCase(6, 3, ExpectedResult = 5)]
+        [TestCase(5, 3, ExpectedResult = 6)]
+        [TestCase(1, 3, ExpectedResult = 6)]
+        [TestCase(0, 3, ExpectedResult = 0)]
+        public int Should_ChangeBackstageQuality_When_Updated(int sellIn, int quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            return Items[0].Quality;
         }
     }
 }
